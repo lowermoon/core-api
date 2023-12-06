991b9e3534408db420f911e93d34313d9711c441
ryturn <watashiox123@gmail.com>
2023-12-06 11:50:06 +0700

    fix bug date

--- models/functions/createRecords.js
+++ models/functions/createRecords.js
@@ -1,10 +1,9 @@
 const loggedRecord = require('../tables/loggedRecords');
 const moment = require('moment-timezone');
 
-let date = moment.tz("Asia/Jakarta").format("YYYY-MM-DD HH:mm:ss")
-console.log(date)
 
 const createRecords = async function (ID,role){
+    let date = moment.tz("Asia/Jakarta").format("YYYY-MM-DD HH:mm:ss")
     loggedRecord.create({
         ID,
         role:role,
